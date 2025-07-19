f50de4e3017eb261af35b2d3abec9f5b4f23f0d8
Pedro Nascimento <pedrohenriquemonteironasciment@gmail.com>
2025-07-19 19:58:45 -0300

    feat(backend): implement application entrypoint and main module

---
+++ backend/src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3001', // Front-end server
+      'http://localhost:3000', // If front-end is running at 3000 port
+      'http://127.0.0.1:3001',
+      'http://127.0.0.1:3000',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
