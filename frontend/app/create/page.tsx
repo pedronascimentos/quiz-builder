4becd346ab152c370d52ad0d9cd79d1e2bff1ab1
Pedro Nascimento <pedrohenriquemonteironasciment@gmail.com>
2025-07-19 20:00:45 -0300

    feat(frontend): build quiz creation page

---
+++ frontend/app/create/page.tsx
@@ -0,0 +1,11 @@
+import { QuizForm } from "@/components/QuizForm"
+
+export default function CreateQuizPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="container mx-auto py-8">
+        <QuizForm />
+      </div>
+    </div>
+  )
+}
